d36ed26d392c8dd26cd735ce78b0eb1befb331bb
PonWer <pontus@werme.org>
2021-03-30 09:58:49 +0200

    Some files

--- IdleMiningGame/Program.cs
+++ IdleMiningGame/Program.cs
@@ -1,12 +1,17 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Blazored.LocalStorage;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using RobotGame.Shared;
 
 namespace IdleMiningGame
 {
@@ -15,11 +20,36 @@ namespace IdleMiningGame
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
 
-            await builder.Build().RunAsync();
+            builder
+                .RootComponents
+                .Add<App>("app");
+
+            builder
+                .Services
+                .AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+            
+            builder
+                .Services
+                .AddSingleton<Game>();
+
+            var host = builder.Build();
+
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
+
+
     }
 }
